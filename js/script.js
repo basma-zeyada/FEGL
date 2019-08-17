442a30ec8cca9972ca82392c45c0560fb85ac2ce
basma-zeyada <basma_zeyada@live.com>
2019-08-17 09:33:32 +0200

    generate PDF

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+
+
 $(function(){
   $(window).on("scroll",function(){
     var top = $(this).scrollTop();
     $(".jumbotron").css("background-position-y",(top/-5));
-    $(".principles").css("background-position-y",(top/-5));
+    // $(".principles").css("background-position-y",(top/-5));
     
   })
   $(".start").on("click",function(e) {
@@ -16,4 +18,24 @@ $(function(){
     $(this).find("i").toggleClass("fa-chevron-down").toggleClass("fa-chevron-up")
     $(this).next(".hint").slideToggle();
   })
+
+  /////// get today's date
+  $("#form_date").val(getDate()[0] + "/" + getDate()[1] + "/" + getDate()[2])
 })
+
+function getDate(){
+  var d = new Date()
+  var day = d.getDate();
+  var month = d.getMonth()+1
+  var year = d.getFullYear();
+  return [day,month,year];
+}
+function getFormData(){
+  var dev_name = document.getElementById('dev_name').value;
+  var element = document.getElementById('form_data');
+  var opt = {
+    margin:       10,
+    filename:     dev_name,
+  };
+  html2pdf(element, opt);
+}
