12ff83e87e79b42d1b93391398e0bfc84f8cf1d1
Basma Zeyada <basma.zeyada@espace.com.eg>
2021-02-20 11:51:44 +0200

    fix manifest typo

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -20,7 +20,6 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '../index.html',
-  './', // Alias for index.html
   '../style/index.css',
   '../js/script.js'
 ];
