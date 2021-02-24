13b9a660192081cd4dea883a87460081b051e3f1
Basma Zeyada <basma.zeyada@espace.com.eg>
2021-02-24 09:00:26 +0200

    handle offline

--- js/pwa.js
+++ js/pwa.js
@@ -1,6 +1,7 @@
 
 // Initialize deferredPrompt for use later to show browser install prompt.
 var deferredPrompt;
+var ua = navigator.userAgent;
 
 function promotePWAInstallation(){
   // Hide the app provided install promotion
@@ -9,6 +10,10 @@ function promotePWAInstallation(){
   deferredPrompt.prompt();
   // We've used the prompt, and can't use it again, throw it away
   deferredPrompt = null;
+}
+
+function updateOnlineStatus(){
+  $(".offline").toggleClass("display-none")
 }
 
 $(function(){
@@ -24,13 +29,27 @@ $(function(){
     });
   }
 
+  // show pwa popup only on safari - ios
+  var isSafari = (
+    ua.indexOf("iPhone") !== -1 &&
+    ua.indexOf("Safari") !== -1 &&
+    ua.indexOf("CriOS") === -1 &&
+    ua.indexOf("FxiOS") === -1
+  );
+
+  if(isSafari){
+    $(".ios-pwa-modal").removeClass("hidden")
+  }
+
   window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the default infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI and show the customized install App promotion
-  $(".pwa-banner").removeClass("hidden")
+  if(!isSafari){
+    $(".pwa-banner").removeClass("hidden")
+  }
   });
 
   window.addEventListener('appinstalled', () => {
@@ -39,4 +58,7 @@ $(function(){
     // Optionally, send analytics event to indicate successful install
     console.log('PWA was installed');
    });
+
+  window.addEventListener('online',  updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
 })
